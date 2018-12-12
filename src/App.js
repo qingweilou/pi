598ebd4c4cb63dd26fac7c2f94079284d4c0cad8
qingwei lou <qingwei_lou@yahoo.com>
2018-12-12 00:04:06 -0700

    minor data changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
               fill: false,
               lineTension: 0.1,
               backgroundColor: 'rgba(75,192,192,0.4)',
-              borderColor: 'rgba(75,192,192,1)',
+              borderColor: 'rgba(75,193,192,1)',
               data: [65,59,80,81,56,55,40]
           }
       ]
